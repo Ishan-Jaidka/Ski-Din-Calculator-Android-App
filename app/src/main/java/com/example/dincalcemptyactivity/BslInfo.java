e14885b6e70ebcd3262471ab2c767dbe701807ec
ijaidka94 <72633250+ijaidka94@users.noreply.github.com>
2020-12-18 19:19:29 -0800

    update

--- app/src/main/java/com/example/dincalcemptyactivity/BslInfo.java
+++ app/src/main/java/com/example/dincalcemptyactivity/BslInfo.java
@@ -1,4 +1,30 @@
 package com.example.dincalcemptyactivity;
 
-public class BslInfo {
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BslInfo extends AppCompatActivity {
+    protected TextView xclosebsl, infobsl;
+    protected ImageView bslImg;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bslinfo);
+
+        xclosebsl = (TextView) findViewById(R.id.xbuttonbsl);
+
+        xclosebsl.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                finish();
+                return false;
+            }
+        });
+
+    }
 }
--- app/src/main/java/com/example/dincalcemptyactivity/TypeInfo.java
+++ app/src/main/java/com/example/dincalcemptyactivity/TypeInfo.java
@@ -1,4 +1,33 @@
 package com.example.dincalcemptyactivity;
 
-public class TypeInfo {
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TypeInfo extends AppCompatActivity {
+    protected TextView xclose, info;
+    protected ImageView skierType;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_typeinfo);
+
+        
+
+        xclose = (TextView) findViewById(R.id.xbutton);
+        skierType = (ImageView) findViewById(R.id.skierTypeInfo);
+
+        xclose.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                finish();
+                return false;
+            }
+        });
+
+    }
 }
